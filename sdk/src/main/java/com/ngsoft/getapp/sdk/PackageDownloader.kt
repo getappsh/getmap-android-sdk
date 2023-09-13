2f94c26e37a6433df8b6d7b13dc0e91019e5c8de
VadimD <VadimD@VADIM-D-TEMP>
2023-09-13 09:51:23 +0300

    post-done fixes to ref #17

--- sdk/src/main/java/com/ngsoft/getapp/sdk/PackageDownloader.kt
+++ sdk/src/main/java/com/ngsoft/getapp/sdk/PackageDownloader.kt
@@ -7,7 +7,6 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import android.os.Environment
 import android.webkit.MimeTypeMap
 import androidx.core.net.toUri
 import java.io.File
