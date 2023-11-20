eefd1c204662a755d43aef4a65cadc84a882d97d
Ari Havlin <ayhavlin@gmail.com>
2023-11-20 14:16:07 +0200

    Create JsonUtils to get the expected checksum

---
+++ sdk/src/main/java/com/ngsoft/getapp/sdk/utils/JsonUtils.kt
@@ -0,0 +1,18 @@
+package com.ngsoft.getapp.sdk.utils
+
+import org.json.JSONObject
+import java.nio.file.Files
+import java.nio.file.Paths
+
+object JsonUtils {
+
+    fun readJson(jsonPath: String): JSONObject{
+        val file = Files.readAllBytes(Paths.get(jsonPath))
+        return JSONObject(String(file))
+    }
+
+    fun getValueFromJson(key: String, jsonPath: String): String{
+        val jsonObject = readJson(jsonPath);
+        return jsonObject.getString(key)
+    }
+}
