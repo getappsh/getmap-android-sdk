94631428927d4de5627b1ed9b7fdb31f6f70632f
Ari Havlin <ayhavlin@gmail.com>
2024-07-16 17:03:42 +0300

    Create function to insert, read and delete records from the content provider

--- example-app/src/main/java/com/example/getmap/matomo/provider/ReportUtils.kt
+++ example-app/src/main/java/com/example/getmap/matomo/provider/ReportUtils.kt
@@ -1,7 +1,7 @@
 package com.example.getmap.matomo.provider
 
-import Report
 import android.annotation.SuppressLint
+import android.content.ContentResolver
 import android.content.ContentValues
 import android.database.Cursor
 import java.text.SimpleDateFormat
@@ -10,7 +10,7 @@ import java.util.Locale
 
 object ReportUtils {
 
-    fun insertReport(db: SQLiteDatabase, report: Report) {
+    fun insertReport(contentResolver: ContentResolver, report: Report) {
         val values = ContentValues().apply {
             put(ReportDatabaseHelper.COLUMN_NAME, report.name)
             put(ReportDatabaseHelper.COLUMN_TYPE, report.type.toString())
@@ -22,7 +22,47 @@ object ReportUtils {
             put(ReportDatabaseHelper.COLUMN_DIMID, report.dimId)
             put(ReportDatabaseHelper.COLUMN_DIMVALUE, report.dimValue)
         }
-        db.insert(ReportDatabaseHelper.TABLE_REPORTS, null, values)
+        contentResolver.insert(ReportProvider.CONTENT_URI, values)
+    }
+
+    fun readReports(contentResolver: ContentResolver): List<Report> {
+        val reports = mutableListOf<Report>()
+        val projection = arrayOf(
+            ReportDatabaseHelper.COLUMN_ID,
+            ReportDatabaseHelper.COLUMN_NAME,
+            ReportDatabaseHelper.COLUMN_TYPE,
+            ReportDatabaseHelper.COLUMN_PATH,
+            ReportDatabaseHelper.COLUMN_TITLE,
+            ReportDatabaseHelper.COLUMN_CATEGORY,
+            ReportDatabaseHelper.COLUMN_ACTION,
+            ReportDatabaseHelper.COLUMN_VALUE,
+            ReportDatabaseHelper.COLUMN_DIMID,
+            ReportDatabaseHelper.COLUMN_DIMVALUE,
+            ReportDatabaseHelper.COLUMN_CREATED_AT
+        )
+
+        val cursor = contentResolver.query(
+            ReportProvider.CONTENT_URI,
+            projection,
+            null,
+            null,
+            null)
+
+        cursor?.use { c ->
+            while (c.moveToNext()) {
+                val report = getReport(c)
+                reports.add(report)
+            }
+        }
+        return reports
+    }
+
+    fun deleteReport(contentResolver: ContentResolver, id: Long) {
+        val selection = "${ReportDatabaseHelper.COLUMN_ID} = ?"
+        val selectionArgs = arrayOf(id.toString())
+        contentResolver.delete(ReportProvider.CONTENT_URI, selection, selectionArgs)
+    }
+
     @SuppressLint("ConstantLocale")
     val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
 
@@ -32,7 +72,7 @@ object ReportUtils {
         }
     }
 
-    fun getReport(cursor: Cursor): Report {
+    private fun getReport(cursor: Cursor): Report {
         val id = cursor.getLong(cursor.getColumnIndexOrThrow(ReportDatabaseHelper.COLUMN_ID))
         val type = VariantReport.fromString(
             cursor.getString(
@@ -51,7 +91,6 @@ object ReportUtils {
         val dimValue = cursor.getString(cursor.getColumnIndexOrThrow(ReportDatabaseHelper.COLUMN_DIMVALUE))
         val createdAtString = cursor.getString(cursor.getColumnIndexOrThrow(ReportDatabaseHelper.COLUMN_CREATED_AT))
         val createdAt = stringToDate(createdAtString)
-
 
         return Report(id, type, path, title, category, action, name, value, dimId, dimValue, createdAt)
     }
