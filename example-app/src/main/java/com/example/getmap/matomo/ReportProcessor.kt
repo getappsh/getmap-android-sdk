0a8d8e8892585713a36587ba8155044c43daa8a7
Ari Havlin <ayhavlin@gmail.com>
2024-07-16 18:27:03 +0300

    Create ReportProcessor

---
+++ example-app/src/main/java/com/example/getmap/matomo/ReportProcessor.kt
@@ -0,0 +1,61 @@
+package com.example.getmap.matomo
+
+import android.content.ContentResolver
+import com.example.getmap.matomo.provider.Report
+import com.example.getmap.matomo.provider.ReportUtils
+import com.example.getmap.matomo.provider.VariantReportEnum
+import org.matomo.sdk.QueryParams
+import org.matomo.sdk.TrackMe
+import org.matomo.sdk.Tracker
+import org.matomo.sdk.extra.TrackHelper
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+object ReportProcessor {
+
+    fun process(tracker: Tracker, contentResolver: ContentResolver) {
+        val reports = ReportUtils.readReports(contentResolver)
+        reports.forEach { report ->
+            buildEvent(report)?.let { tracker.track(it) }
+            ReportUtils.deleteReport(contentResolver, report.id)
+        }
+
+    }
+
+    private fun buildEvent(report: Report): TrackMe? {
+        val trackBuilder = if (report.dimId != null && report.dimValue != null) {
+            TrackHelper.track().dimension(report.dimId, report.dimValue)
+        } else {
+            TrackHelper.track()
+        }
+
+        val trackMe = when (report.type) {
+            VariantReportEnum.Event -> {
+                report.category ?: return null
+                report.action ?: return null
+                trackBuilder.event(report.category, report.action)
+                    .apply {
+                        report.path?.let { path(it) }
+                        report.name?.let { name(it) }
+                        report.value?.let { value(it) }
+                    }
+                    .build()
+            }
+            VariantReportEnum.Screen -> {
+                trackBuilder.screen(report.path ?: return null)
+                    .apply {
+                        report.title?.let { title(it) }
+                    }
+                    .build()
+            }
+        }
+
+        trackMe?.set(
+            QueryParams.DATETIME_OF_REQUEST,
+            SimpleDateFormat("yyyy-MM-dd HH:mm:ssZ", Locale.US).format(report.createdAt ?: Date())
+        )
+
+        return trackMe
+    }
+}
