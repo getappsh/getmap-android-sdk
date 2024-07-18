021b675c9b78af0eec4433d02e9283c815213cb2
GitHub Action <action@github.com>
2024-07-18 14:12:46 +0000

    New version v2.0.7-CM

--- example-app/build.gradle.kts
+++ example-app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.example.getmap"
         minSdk = 26
         targetSdk = 33
-        versionCode = 8
-        versionName = "2.0.6"
+        versionCode = 9
+        versionName = "2.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
