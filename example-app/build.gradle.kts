e31b5dbd20bf96f31695fc4b3da2bdfae8548099
GitHub Action <action@github.com>
2024-11-21 16:29:48 +0000

    New version v2.2.11-CM

--- example-app/build.gradle.kts
+++ example-app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.example.getmap"
         minSdk = 26
         targetSdk = 33
-        versionCode = 26
-        versionName = "2.2.10"
+        versionCode = 27
+        versionName = "2.2.11"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
