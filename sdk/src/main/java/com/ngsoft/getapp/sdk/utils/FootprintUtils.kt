7e5a2c22a715267219cd839f79e5bcf4833481be
Elazar <lzrprws@gmail.com>
2025-07-28 12:21:18 +0300

    Refactor: Handle MultiPolygon footprints and improve date formatting
    The `FootprintUtils.toString()` method now supports "MultiPolygon" geometry types in addition to "Polygon". Error handling has been added for unknown geometry types and parsing errors.
    
    The `formatDate` method in `DownloadListAdapter` now handles null or blank input dates and includes error logging for parsing failures.

--- sdk/src/main/java/com/ngsoft/getapp/sdk/utils/FootprintUtils.kt
+++ sdk/src/main/java/com/ngsoft/getapp/sdk/utils/FootprintUtils.kt
@@ -1,31 +1,52 @@
 package com.ngsoft.getapp.sdk.utils
 
+import android.util.Log
 import org.json.JSONObject
 
 internal object FootprintUtils {
 
-    fun toString(footprint: JSONObject): String{
-        val coordinatesArray = footprint
-            .getJSONArray("coordinates")
-            .getJSONArray(0) // Assuming there's only one set of coordinates in the example
+    fun toString(footprint: JSONObject): String {
+        return try {
+            val type = footprint.getString("type")
+            val coordinatesArray = footprint.getJSONArray("coordinates")
+            val coordinatesString = StringBuilder()
 
-        val coordinatesString = StringBuilder()
-
-        for (i in 0 until coordinatesArray.length()) {
-            val innerArray = coordinatesArray.getJSONArray(i)
-            for (j in 0 until innerArray.length()) {
-                val pointArray = innerArray.getJSONArray(j)
-                val longitude = pointArray.getDouble(0)
-                val latitude = pointArray.getDouble(1)
-
-                coordinatesString.append("$longitude,$latitude,")
+            if (type == "Polygon") {
+                val ringArray = coordinatesArray.getJSONArray(0)
+                for (i in 0 until ringArray.length()) {
+                    val pointArray = ringArray.getJSONArray(i)
+                    val longitude = pointArray.getDouble(0)
+                    val latitude = pointArray.getDouble(1)
+                    coordinatesString.append("$longitude,$latitude,")
+                }
+            } else if (type == "MultiPolygon") {
+                for (i in 0 until coordinatesArray.length()) {
+                    val polygonArray = coordinatesArray.getJSONArray(i)
+                    val ringArray = polygonArray.getJSONArray(0)
+                    for (j in 0 until ringArray.length()) {
+                        val pointArray = ringArray.getJSONArray(j)
+                        val longitude = pointArray.getDouble(0)
+                        val latitude = pointArray.getDouble(1)
+                        coordinatesString.append("$longitude,$latitude,")
+                    }
+                }
+            } else {
+                Log.e("FootprintUtils", "Unknown geometry type: $type")
+                return ""
             }
-        }
 
-        // Remove the trailing comma
-        coordinatesString.deleteCharAt(coordinatesString.length - 1)
-        return coordinatesString.toString()
+            if (coordinatesString.isNotEmpty()) {
+                coordinatesString.deleteCharAt(coordinatesString.length - 1)
+            }
+
+            return coordinatesString.toString()
+
+        } catch (e: Exception) {
+            Log.e("FootprintUtils", "Error parsing footprint: ${e.message}", e)
+            ""
+        }
     }
+
 
     fun toList(footprint: String): List<Double> = footprint.split(",").map { it.toDouble() }
 
